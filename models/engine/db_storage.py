79c597d23cb3faa19fb577cfbe9d76562c262cef
AdrianaAriza <1296@holbertonschool.com>
2020-04-01 19:06:09 -0500

    [Fix]:

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -33,7 +33,7 @@ class DBStorage():
         """
         new_dict = {}
         for c in classes:
-            if cls is None or cls is classes[c] or cls is c:
+            if cls is None:
                 objs = self.__session.query(classes[c]).all()
                 for obj in objs:
                     key = obj.__class__.__name__ + '.' + obj.id
